33c94a31379eb963dd99407ee51aa6e4507959d9
Aaron Akbari-Mort and Adriana C <adriana@>
2018-03-27 15:58:59 -0700

    add basic HTML structure plus Javascript to perform form function

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,24 @@
-var reducer = (accumulator, currentValue) => accumulator + currentValue;
+$().ready(function() {
+  $("form#factorial").submit(function(event) {
+    event.preventDefault();
 
-// helper function to check if a number in a certain range
-Number.prototype.between = function(a, b) {
-  var min = Math.min(a, b),
-    max = Math.max(a, b);
+    var total = 1;
+    var userInput = parseInt($("#number").val());
+    if (userInput === 0) {
+      total = 1;
+    } else if (userInput < 0) {
+      // $("#output").append("Please enter a number larger than zero.");
+      total = "Please enter a number zero or larger."
+    }
+    for(var index = userInput; index > 0; index -= 1) {
+      total *= index;
+    }
 
-  return this > min && this < max;
-};
+    $("#output").append("<br><p class='lead'>Your factorial output is: " + "</p>" + "<h1>" + total + "</h1>");
+    $("#factorial").hide();
 
-
-// function to decide the stressLevel, return copingMethod
-// score : a positive int
-// copingMethods: an array of string with stressLevel words
-var stressIdentifier = function(score, copingMethods){
-  var stressLevel = '';
-  if (score.between(0,5)){
-    stressLevel = copingMethods[0];
-  }else if(score.between(5,10)){
-    stressLevel = copingMethods[1];
-  }else{
-    stressLevel = copingMethods[2];
-  }
-  return stressLevel;
-}
-
-
-// user interface logic
-$().ready(function(){
-  $("#stressTest").submit(function(e){
-    e.preventDefault();
-
-    var score = 0;
-    $("input:checkbox[name=warningSigns]:checked").each(function(){
-      score += parseInt($(this).val());
+    $("#clear").click(function() {
+      location.reload();
     });
-    var copingMethods = ['Sleep more','relax more', 'go to see some plants'];
-    var stressLevel = stressIdentifier(score, copingMethods);
-
-    var score2 = 0;
-    $("input:checkbox[name=warningSigns]:checked").each(function(){
-      score2 += parseInt($(this).val());
-    });
-    var copingMethods2 = ['Sleep','relax', 'see some plants'];
-    var stressLevel2 = stressIdentifier(score, copingMethods2);
-
-    $("#copingMethods").show();
-    $("#copingMethods").append("<p class='lead'>Here goes your coping methods: <br>"+stressLevel+"</p>");
-    $("#copingMethods").append("<p class='lead'>"+stressLevel2+"</p>");
-
   });
-
 });
